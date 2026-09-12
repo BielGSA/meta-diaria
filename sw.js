9a8d4cfe77f9d8c4cb2b17f2dcc91d299690deb3
BielGSA <jgabrielhinode@gmail.com>
2026-09-12 19:18:48 -0300

    Atualiza cache do histórico por plataforma

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="drivemeta-v13-mobile-final-5";
+const CACHE="drivemeta-v13-mobile-final-6";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./tk27-labs.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
